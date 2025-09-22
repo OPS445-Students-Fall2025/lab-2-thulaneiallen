277aeb50210083afbb6330a240b586e5ccb54441
Thulanei Allen <tallen11@myseneca.ca>
2025-09-21 22:15:28 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Thulanei Allen
+# Author ID: tallen11
+# Date Created: 2025/09/21
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
